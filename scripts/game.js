d3962fa7836139a4434c97a8a04d96e29aad430d
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2016-04-20 11:12:36 -0400

    Sets a threshold on the number of shots that can be fired in succession.

--- scripts/game.js
+++ scripts/game.js
@@ -4,6 +4,8 @@ require(["ship", "key", "background"], function(a, b, c){
 	var canvasHeight = canvas.getAttribute("height");
 	var context = canvas.getContext("2d");
 	var FPS = 30;
+	var framesSinceLastFired = 0;
+	var shipFireThreshold = 5;
 
 	var ship = new Ship(canvasWidth, canvasHeight, context);
 	var background = new Background(canvasWidth, canvasHeight, context);
@@ -27,9 +29,15 @@ require(["ship", "key", "background"], function(a, b, c){
 		}
 		ship.clamp();
 
+		framesSinceLastFired++;
+
 		if(Key.isDown(Key.SPACE)) {
-			projectiles.push(ship.shoot());
+			if(framesSinceLastFired > shipFireThreshold) {
+				projectiles.push(ship.shoot());
+				framesSinceLastFired = 0;
+			}
 		}
+
 		for(var i = 0; i < projectiles.length; i++) {
 			projectiles[i].update();
 		}
